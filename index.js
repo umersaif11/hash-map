1f38ca3bfa797ecb5e14fe9280de1a99e51792d6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-10-17 18:03:50 +0500

    Add the set-value pairs for testing

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import { hashMap } from "./hashmap.js"
 
-let hashmap = new hashMap();
-console.log(hashmap.hash('umar'));
+let test = new hashMap();
+
+ test.set('apple', 'red')
+ test.set('banana', 'yellow')
+ test.set('carrot', 'orange')
+ test.set('dog', 'brown')
+ test.set('elephant', 'gray')
+ test.set('frog', 'green')
+ test.set('grape', 'purple')
+ test.set('hat', 'black')
+ test.set('ice cream', 'white')
+ test.set('jacket', 'blue')
+ test.set('kite', 'pink')
+ test.set('lion', 'golden')
